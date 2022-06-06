5be228895527244aa4edefb315c659d907f19102
Mylodie <mylodie7@gmail.com>
2022-06-06 21:00:19 +0800

    Add init-arch.sh

---
+++ init-arch.sh
@@ -0,0 +1,15 @@
+pacman -S --noconfirm \
+	base-devel git curl wget vim tmux zsh cronie wireguard-tools chezmoi \
+    exa ripgrep fzf htop bat duf fd jq procs tree starship \
+	nodejs npm python python-pip go \
+	docker docker-compose 
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+chezmoi init --apply https://github.com/Mylodie/dotfiles.git
+
+chsh -s $(which zsh)

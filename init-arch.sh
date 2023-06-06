c6fc4a0afc530a1116a1817959d7b7ff85b10d18
Mylodie <mylodie7@gmail.com>
2023-06-06 11:38:15 +0800

    feat: pacman source

--- init-arch.sh
+++ init-arch.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-while getopts "h:g:" opt; do
+
+while getopts "h:g:s" opt; do
   case $opt in
-    h) echo "Usage: $0 [-g GIT_SERVICE_URL]"; exit 0;;
+    h) echo "Usage: $0 [-g GIT_SERVICE_URL] [-s]"; exit 0;;
     g) GIT_SERVICE_URL=$OPTARG;;
-    *) echo "Usage: $0 [-g GIT_SERVICE_URL]"; exit 1;;
+    c) USE_CHINA_SOURCE=true;;
+    *) echo "Usage: $0 [-g GIT_SERVICE_URL] [-s]"; exit 1;;
   esac
 done
 
 if [ -z "$GIT_SERVICE_URL" ]; then
   GIT_SERVICE_URL="https://github.com"
+fi
+
+if [ "$USE_CHINA_SOURCE" = true ]; then
+  sudo sed -i 's/^#Server/Server/g' /etc/pacman.d/mirrorlist
+  sudo sed -i '1i Server = http://mirrors.163.com/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
+  sudo sed -i '2i Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
 fi
 
 pacman -S --noconfirm \
@@ -23,8 +31,6 @@ git clone "${GIT_SERVICE_URL}/zsh-users/zsh-autosuggestions.git" ${ZSH_CUSTOM:-~
 
 git clone "${GIT_SERVICE_URL}/zsh-users/zsh-syntax-highlighting.git" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-
 chezmoi init --apply "${GIT_SERVICE_URL}/Mylodie/dotfiles.git"
 
-
-chsh -s $(which zsh)%
+chsh -s "$(which zsh)"

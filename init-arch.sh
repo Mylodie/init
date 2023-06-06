bf13eddc373d218080ad29ffb99a9196be5501c1
Mylodie <mylodie7@gmail.com>
2023-06-06 11:22:37 +0800

    feat: abstruct git service url

--- init-arch.sh
+++ init-arch.sh
@@ -1,15 +1,30 @@
+#!/bin/bash
+while getopts "h:g:" opt; do
+  case $opt in
+    h) echo "Usage: $0 [-g GIT_SERVICE_URL]"; exit 0;;
+    g) GIT_SERVICE_URL=$OPTARG;;
+    *) echo "Usage: $0 [-g GIT_SERVICE_URL]"; exit 1;;
+  esac
+done
+
+if [ -z "$GIT_SERVICE_URL" ]; then
+  GIT_SERVICE_URL="https://github.com"
+fi
+
 pacman -S --noconfirm \
     base-devel git curl wget vim tmux zsh cronie wireguard-tools chezmoi \
     exa ripgrep fzf htop bat duf fd jq procs tree starship \
     nodejs npm python python-pip go \
-    docker docker-compose 
+    docker docker-compose
 
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone "${GIT_SERVICE_URL}/ohmyzsh/ohmyzsh.git" ~/.oh-my-zsh
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone "${GIT_SERVICE_URL}/zsh-users/zsh-autosuggestions.git" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone "${GIT_SERVICE_URL}/zsh-users/zsh-syntax-highlighting.git" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-chezmoi init --apply https://github.com/Mylodie/dotfiles.git
 
-chsh -s $(which zsh)
+chezmoi init --apply "${GIT_SERVICE_URL}/Mylodie/dotfiles.git"
+
+
+chsh -s $(which zsh)%
